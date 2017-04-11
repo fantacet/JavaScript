a5cba42b70cec2dbe78ef7400a2b2d967926380a
Ta-Chun Chien <jdj0125@gmail.com>
2017-04-11 17:25:27 +0800

    20170411-1

---
+++ Demo/Lab08/get.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MSITJSLab_Lab07_get : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //string str = Request.QueryString["txtName"];   //get
+        //string str = Request.Form["txtName"];  //post
+        string str = Request.Params["txtName"];  //get,post
+        Response.Write(str);
+    }
+}
